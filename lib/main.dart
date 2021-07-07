c4a1ddd3f7b71254c10954858680244227bbc14d
Josh Austin <josh.has.ideas@icloud.com>
2021-07-07 00:59:57 -0500

    first working draft

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class BongosApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: BongoPage(title: 'Flutter Demo Home Page'),
+      home: BongoPage(title: 'Beat the Bongos'),
     );
   }
 }
@@ -22,18 +22,40 @@ class BongoPage extends StatefulWidget {
   BongoPage({Key? key, required this.title}) : super(key: key);
 
   final String title;
+  final player = AudioCache();
 
   @override
   _BongoPageState createState() => _BongoPageState();
+}
+
+Widget bongo(BongoPage widget, String path) {
+  return Expanded(
+    child: TextButton(
+      child: Image.asset("assets/bongo.png"),
+      onPressed: () {
+        widget.player.play(path);
+      },
+    )
+  );
 }
 
 class _BongoPageState extends State<BongoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center());
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Row(
+          children: [
+            bongo(widget, "bongo1.wav"),
+            bongo(widget, "bongo2.wav"),
+            bongo(widget, "bongo3.wav"),
+            bongo(widget, "bongo4.wav")
+          ]
+        )
+      )
+    );
   }
 }
